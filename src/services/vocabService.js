e7f4614791ac2171f2d93cd0962e256d204cbd4c
Aflah Agus Rizkika <raaflahar@gmail.com>
2025-07-07 14:51:34 +0700

    feat: (aflah) add VocabService and VocabularyDashboard

--- src/services/vocabService.js
+++ src/services/vocabService.js
@@ -1,13 +1,36 @@
-// src/services/vocabService.js
-import axiosInstance from "../utils/axiosInstance"; // pastikan sudah setup axiosInstance
+
+import axiosInstance from "../utils/axiosInstance";
+
+export const uploadVocabularies = async (file) => {
+  const formData = new FormData();
+  formData.append("file", file);
+  const response = await axiosInstance.post("/vocabularies/upload", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return response.data;
+};
 
 
 export const fetchVocabCategories = async () => {
   const response = await axiosInstance.get("/vocabularies/categories");
-  return response.data.data; // assumed backend response structure
+  const categoryNames = response.data.data;
+
+  return categoryNames.map(name => ({
+    id: name,   
+    name: name, 
+  }));
 };
 
 export const fetchVocabulariesByCategory = async (category) => {
   const response = await axiosInstance.get(`/vocabularies?category=${category}`);
+  console.log(response);
   return response.data.data;
+};
+
+
+export const deleteVocabulary = async (vocabularyId) => {
+  const response = await axiosInstance.delete(`/vocabularies/${vocabularyId}`);
+  return response.data;
 };
